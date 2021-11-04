fd34553bd06ca7e594aa62100c743925fc60e615
Kwontaehyun <kygkth2011@gmail.com>
2021-11-05 01:25:31 +0900

    feature/movie_info - develop feature

--- flex/views/movies_views.py
+++ flex/views/movies_views.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, url_for, render_template
 from werkzeug.utils import redirect
+from flex.models import Movie
 
 bp = Blueprint('movies', __name__, url_prefix='/movies')
 
 #영화 목록 페이지
 @bp.route('/')
 def list():
-    return render_template('client_templates/movies-list.html')
+    movie_list = Movie.query.order_by(Movie.id.desc())
+    return render_template('client_templates/movies-list.html', movie_list = movie_list)
 
 #영화 목록 페이지에서 영화 이름 누르면 해당 영화 detail 페이지 랜더링, 각 영화의 이미지, 이름 등 db에 있는 정보들을 인자로 넘겨서 랜더링
-'''
-@bp.route('/detail/<여기에 db 영화테이블 id 참조>')
-def detail(영화테이블.id):
-    디비에서 영화 이름등 정보 갖고오고, 
-    이미지도 갖고오고
-    return render_template('client_templates/movie-info-sidebar-right.html', 인자1, 인자2, 인자3,..)
-    '''
+
+@bp.route('/detail/<int:movie_id>')
+def detail(movie_id):
+    movie = Movie.query.get(movie_id)
+    return render_template('client_templates/movie-info-sidebar-right.html', movie = movie)
