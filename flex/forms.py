a452386445388146021eee2819dce77188b661c2
TaehyungKim <netchip8@naver.com>
2021-11-04 13:09:09 +0900

    client/login_signup_logout

--- flex/forms.py
+++ flex/forms.py
@@ -14,7 +14,7 @@ class MemberCreateForm(FlaskForm):
     id = StringField('아이디', validators=[DataRequired('아이디는 필수입력 항목입니다.'), Length(min=3, max=25)])
     password1 = PasswordField('비밀번호', validators=[
         DataRequired('비밀번호는 필수입력 항목입니다.'), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
-    password2 = PasswordField('비밀번호확인', validators=[DataRequired('비밀번호를 한번 더 입력해주세요.')])
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired('비밀번호를 한번 더 입력해주세요.')])
     birth_date = StringField('생년월일', validators=[DataRequired('생년월일은 필수입력 항목입니다.')])
     phone = StringField('휴대전화', validators=[DataRequired('휴대전화는 필수입력 항목입니다.')])
     email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email()])
