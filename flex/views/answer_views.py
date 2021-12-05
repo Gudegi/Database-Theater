ba02c65e98929cc820d008ce056d3d2dcd79dca7
hyeongi kim <70681582+kim-hyeon-gi@users.noreply.github.com>
2021-12-06 02:57:40 +0900

    feature services complete

--- flex/views/answer_views.py
+++ flex/views/answer_views.py
@@ -4,22 +4,22 @@ from werkzeug.utils import redirect
 
 from .. import db
 from ..forms import AnswerForm,NoticeAnswerForm
-from ..models import Question, Answer,Notice,NoticeAnswer
+from ..models import Notice,NoticeAnswer,InquiryAnswer,Inquiry
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 
-@bp.route('/create/<int:question_id>', methods=('POST',))
-def create(question_id):
+@bp.route('/create/<int:inquiry_id>', methods=('POST',))
+def create(inquiry_id):
     form = AnswerForm()
-    question = Question.query.get_or_404(question_id)
+    inquiry = Inquiry.query.get_or_404(inquiry_id)
     if form.validate_on_submit():
         content = request.form['content']
-        answer = Answer(content=content, create_date=datetime.now())
-        question.qsa.append(answer)
+        answer = InquiryAnswer(content=content, create_date=datetime.now())
+        inquiry.inquiry.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
-    return render_template('question/question_detail.html', question=question, form=form)
+        return redirect(url_for('services.inquiryDetail', inquiry_id=inquiry_id))
+    return render_template('question/question_detail.html', inquiry=inquiry, form=form)
 
 @bp.route('/noticeCreate/<int:notice_id>', methods=('POST',))
 def noticeCreate(notice_id):
--- flex/views/services_views.py
+++ flex/views/services_views.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, url_for,g
+from flask import Blueprint, render_template, request, url_for,g,flash
 from werkzeug.utils import redirect
 
 from .. import db
 from flex.models import Notice,Inquiry,InquiryAnswer,NoticeAnswer,Question,Answer
 from ..forms import QuestionForm, AnswerForm,NoticeForm,NoticeAnswerForm
-
+from .auth_views import login_required
 bp = Blueprint('services', __name__, url_prefix='/services')
 
 @bp.route('/')
@@ -46,25 +46,37 @@ def noticeDetail(notice_id):
 @bp.route('/inquiry/')
 def _inquiry():
     page = request.args.get('page', type=int, default=1)
-    inquiry_list = Inquiry.query.order_by(Inquiry.create_date.desc())
+    inquiry_list = Inquiry.query.order_by(Inquiry.date.desc())
     inquiry_list = inquiry_list.paginate(page, per_page=10)
-    return render_template('services/services_question_list.html', Inquiry_list=inquiry_list)
+    return render_template('services/services_question_list.html', inquiry_list=inquiry_list)
 
 
 
 @bp.route('/inquiry/create/', methods=('GET', 'POST'))
+@login_required
 def inquiryCreate():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now(),member_id= 0  )
-        db.session.add(question)
+        inquiry = Inquiry(title=form.subject.data, content=form.content.data, date=datetime.now(),member_id = g.member.id)
+        db.session.add(inquiry)
         db.session.commit()
         return redirect(url_for('services._inquiry'))
     return render_template('question/question_form.html', form=form)
 
 
-@bp.route('/inquiry/<int:question_id>/')
-def inquiryDetail(question_id):
+@bp.route('/inquiry/<int:inquiry_id>/')
+def inquiryDetail(inquiry_id):
     form = AnswerForm()
-    question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question, form=form)
+    inquiry = Inquiry.query.get_or_404(inquiry_id)
+    return render_template('question/question_detail.html', inquiry=inquiry, form=form)
+
+@bp.route('/delete/<int:inquiry_id>')
+@login_required
+def inquiryDelete(inquiry_id):
+    inquiry = Inquiry.query.get_or_404(inquiry_id)
+    if g.member.id != inquiry.member_id:
+        flash('삭제권한이 없습니다')
+        return redirect(url_for('services.inquiryDetail', inquiry_id=inquiry_id))
+    db.session.delete(inquiry)
+    db.session.commit()
+    return redirect(url_for('services._inquiry'))
